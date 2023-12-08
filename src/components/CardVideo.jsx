f37854c6cfc1fa391a5a6524660e5576f50d7f6a
Sergiioog <sergiogargor9@gmail.com>
2023-12-09 00:02:12 +0100

    Index y retoques

--- src/components/CardVideo.jsx
+++ src/components/CardVideo.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 
 const CardVideo = (props) => {
    
-    const { titulo, urlVideo } = props;
+    const { titulo, urlFoto,alt } = props;
 
     return (
         <div className="w-[30vw] h-[30vh] m-10 relative">
-           <video className="w-full h-full object-cover z-0" loop autoPlay muted preload="auto">
-                <source src={urlVideo} type='video/mp4' />
-            </video>
-            <div id="tarj" className="absolute top-0 left-0 w-full h-full flex justify-center items-center z-1">
-            <p className="bg-opacity-60 shadow-md backdrop-blur-sm backdrop-filter border border-opacity-20 rounded-md
-            text-xl bg-slate-50 h-20 w-56 flex justify-center items-center text-center transform transform-origin-center transition-transform duration-300 ease-in-out hover:scale-105
-            hover:cursor-pointer">{titulo}</p>
-
-            </div>
+           <figure className="w-full h-full object-cover flex justify-center items-center">
+                <img src={urlFoto} alt={alt} className='max-w-full max-h-full' />
+            </figure>
+            <h3 className='flex justify-center items-center text-2xl text-white'>{titulo}</h3>
         </div>
     );
+    
 }
+
 
 export default CardVideo;
 
