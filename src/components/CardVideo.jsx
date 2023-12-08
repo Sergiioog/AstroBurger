fbf209d15ff888b27b038bbd8ba4020236b5c44d
Sergiioog <sergiogargor9@gmail.com>
2023-12-09 00:40:39 +0100

    Productos

--- src/components/CardVideo.jsx
+++ src/components/CardVideo.jsx
@@ -5,11 +5,11 @@ const CardVideo = (props) => {
     const { titulo, urlFoto,alt } = props;
 
     return (
-        <div className="w-[30vw] h-[30vh] m-10 relative">
+        <div className="w-[35vw] h-[35vh] m-10 relative">
            <figure className="w-full h-full object-cover flex justify-center items-center">
                 <img src={urlFoto} alt={alt} className='max-w-full max-h-full' />
             </figure>
-            <h3 className='flex justify-center items-center text-2xl text-white'>{titulo}</h3>
+            <h3 className='flex justify-center items-center text-3xl text-white'>{titulo}</h3>
         </div>
     );
     
